c50f2bf015389d34370c75af6d53f6e9db601cc7
ana64 <66880492+ana64@users.noreply.github.com>
2024-05-28 23:17:42 +0200

    implement custom truncate pipe and use it in a Movies component

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
-
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies.component';
 import { MoviesService } from '../services/movies.service';
+import {TruncatePipe} from '../truncate.pipe'
+
 
 
 @NgModule({
   declarations: [
-    MoviesComponent
+    MoviesComponent,
+    TruncatePipe
   ],
   imports: [
     CommonModule,
     SharedModule,
-    MoviesRoutingModule,
+    MoviesRoutingModule
   ],
   providers:[
-    MoviesService
+    MoviesService,
+    TruncatePipe
   ]
 })
 export class MoviesModule { }
